9648c442df038467305867ff64ccb77b23fac9b6
William Zhang <39932068+WilliamHYZhang@users.noreply.github.com>
2018-08-17 21:10:02 -0400

    Add files via upload

---
+++ Contests/2018/January/Bronze/newoutofplace.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+size_t unsortedElem(size_t positions[], size_t posSize){
+	
+}
+	
+
+int main(){
+	//cout << "starting" << endl;
+	ifstream fin("outofplace.in");
+	size_t posSize;
+	fin >> posSize;
+	size_t positions[posSize];
+	for(size_t i = 0; i < posSize; ++i)
+		fin >> positions[i];
+
+	size_t bessieIndex;
+	if(positions[0] > positions[1]) 
+		bessieIndex = 0;
+	else if(positions[posSize-1] < positions[posSize-2]) 
+		bessieIndex = posSize-1;
+	else{
+		for(size_t i = 1; i < posSize-1; ++i){
+			if((positions[i-1] < positions[i] && positions[i] > positions[i+1]) && positions[i-1] <= positions[i+1] || (positions[i-1] > positions[i] && positions[i] < positions[i+1] && positions[i-1] <= positions[i+1])){
+				bessieIndex = i;
+				break;
+			}
+		}
+	}
+	size_t bessie = positions[bessieIndex];
+
+	//cout << "bessie and bessie index found" << endl;
+	//cout << "bessie is " << bessie << endl;
+
+	size_t end = 0;
+	for(size_t i = 0; i < posSize; ++i){
+		if(bessie < positions[i]){
+			if(i < bessieIndex)
+				end = i;
+			else{
+				for(size_t j = i-1; j > 0; --j){
+					if(positions[j] != bessie){
+						end = j;
+						break;
+					}
+				}
+			}
+		}
+		if(end != 0) break;
+	}
+	
+	//cout << "end found" << endl;
+	//cout << "end is" << end << endl;
+
+	size_t swaps;
+	if(bessieIndex < end){
+		swaps = end - bessieIndex;
+		for(size_t i = bessieIndex+1; i < end; ++i)
+			if(positions[i] == positions[i+1]) --swaps;
+	}
+	else{
+		swaps = bessieIndex - end;
+		for(size_t i = bessieIndex-1; i > end; --i)
+			if(positions[i] == positions[i-1]) --swaps;
+	}
+
+	//cout << "answer found" << endl;
+	ofstream fout("outofplace.out");
+	fout << swaps << endl;
+	//cout << swaps << endl;
+}
