5c2f0ed8b1e4ca832701b6acea33b9cbafc6a405
William Zhang <39932068+WilliamHYZhang@users.noreply.github.com>
2019-08-28 20:26:40 -0400

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+int main(){
+
+}
+
+//Keep It Simple, Stupid!
