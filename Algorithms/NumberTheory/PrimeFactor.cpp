01dea9a98e80f0d6855df7277337a7890b7f5164
William Zhang <williamhyzhang@gmail.com>
2019-10-06 21:47:29 -0400

    minor

--- Algorithms/NumberTheory/PrimeFactor.cpp
+++ Algorithms/NumberTheory/PrimeFactor.cpp
@@ -3,13 +3,13 @@ vector<int> pfs;
 int tmp = N;
 if(tmp%2 == 0){
 	while(tmp%2 == 0)
-		{pfs.pb(2); tmp /= 2;}
+		{pfs.pub(2); tmp /= 2;}
 }
 for(int i = 3; i <= sqrt(tmp); i += 2){
 	if(tmp%i == 0){
 		while(tmp%i == 0)
-			{pfs.pb(i); tmp /= i;}
+			{pfs.pub(i); tmp /= i;}
 	}
 }
 if(tmp > 2)
-	pfs.pb(tmp);
+	pfs.pub(tmp);
--- Algorithms/NumberTheory/PrimeFactorUnique.cpp
+++ Algorithms/NumberTheory/PrimeFactorUnique.cpp
@@ -2,16 +2,16 @@
 vector<int> pfs;
 int tmp = N;
 if(tmp%2 == 0){
-	pfs.pb(2);
+	pfs.pub(2);
 	while(tmp%2 == 0)
 		tmp /= 2;
 }
 for(int i = 3; i <= sqrt(tmp); i += 2){
 	if(tmp%i == 0){
-		pfs.pb(i);
+		pfs.pub(i);
 		while(tmp%i == 0)
 			tmp /= i;
 	}
 }
 if(tmp > 2)
-	pfs.pb(tmp);
+	pfs.pub(tmp);
