694e8fb0f4bf6ae5bf23b797fdd017ab0d618aa5
William Zhang <WilliamHYZhang@gmail.com>
2020-04-02 13:52:36 -0400

    template stuff

---
+++ GoogleCodeJam/gcj_template.cpp
@@ -0,0 +1,47 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#define mp make_pair
+#define pub push_back
+#define puf push_front
+#define pob pop_back
+#define pof pop_front
+#define ins insert
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define sz(x) (size_t)x.size()
+#define all(x) begin(x), end(x)
+#define rsz resize
+
+const ll INF = 1e18;
+const ld PI = 4*atan((ld)1);
+const int MOD = 1e9 + 7;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ook order_of_key
+#define fbo find_by_order
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;
+	cin >> T;
+	for(int i = 1; i <= T; ++i){
+		cout << "Case #" << i << ": " << "\n";
+	}
+}
+//Keep It Simple, Stupid!
